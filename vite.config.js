f861d4e28c97bc1be73f1f6644fa815321134d09
Armaan Siddiqui <148369512+ARMAANSIDDIQUI@users.noreply.github.com>
2025-08-23 01:54:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-  port: 5000,
-  host: true,
-  cors: {
-    origin: [
-      'https://houseofspecials.in', 
-      'https://www.houseofspecials.in', 
-      'http://localhost'
+    port: 4173,
+    strictPort: true,
+    allowedHosts: [
+      'houseofspecials.in',
+      'www.houseofspecials.in',
+      'localhost'
     ],
   },
-  strictPort: true
-}
-
 })
